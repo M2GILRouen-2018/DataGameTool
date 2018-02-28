dfa6dca0838ce9fcc085139353dfd47b01133ef8
Caron Franck <franck.caron76@gmail.com>
2018-02-28 13:35:33 +0100

    (feature) : Defining sensors and their repository

---
+++ src/main/java/repository/SensorRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import io.univ.rouen.m2gil.smartclass.core.datagenerator.DataGeneratorRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SensorRepository extends DataGeneratorRepository {
+
+}
