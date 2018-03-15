c65ef14a9ab1f94c191b00ed44e924a978c96131
Caron Franck <franck.caron76@gmail.com>
2018-03-15 13:31:18 +0100

    (clean) : Removing unneccessary entity classes.

--- src/main/java/model/entity/Sensor.java
+++
@@ -1,9 +0,0 @@
-package model.entity;
-
-import io.univ.rouen.m2gil.smartclass.core.datagenerator.DataGenerator;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Sensor extends DataGenerator {
-}
